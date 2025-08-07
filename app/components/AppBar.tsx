69b2643137764973d4e74489056e34472680368c
Sid <siddharthmethiya180@gmail.com>
2025-08-08 03:18:25 +0530

    Completed landing page

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,26 +1,87 @@
 'use client'
+import { Music } from 'lucide-react'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 export function AppBar() {
    const session = useSession()
    return (
-      <div className="flex justify-between">
-         <div>Muzer</div>
-         <div>
+      <header className="mx-5 px-4 lg:px-6 h-14 flex items-center">
+         <nav className="flex justify-between w-full">
+            <Link className="flex items-center justify-center" href="#">
+               <Music className="h-6 w-6 text-purple-600" />
+               <span className="ml-2 text-lg font-bold text-purple-400">
+                  Muzer
+               </span>
+            </Link>
+
             {session.data?.user && (
                <button
-                  className="m-2 p-2 bg-blue-300"
-                  onClick={() => signOut()}
+                  onClick={() => {
+                     signOut()
+                  }}
+                  className="text-xs font-medium  text-white  bg-purple-600 px-3 py-2 rounded-md cursor-pointer"
                >
                   Logout
                </button>
             )}
             {!session.data?.user && (
-               <button className="m-2 p-2 bg-blue-300" onClick={() => signIn()}>
-                  SignIn
+               <button
+                  onClick={() => {
+                     signIn()
+                  }}
+                  className="text-xs font-medium  text-white  bg-purple-600 px-3 py-2 rounded-md cursor-pointer"
+               >
+                  Signin
                </button>
             )}
-         </div>
-      </div>
+         </nav>
+      </header>
    )
+}
+
+{
+   /* <div> */
+}
+{
+   /*    {session.data?.user && ( */
+}
+{
+   /*       <button */
+}
+{
+   /*          className="m-2 p-2 bg-blue-300" */
+}
+{
+   /*          onClick={() => signOut()} */
+}
+{
+   /*       > */
+}
+{
+   /*          Logout */
+}
+{
+   /*       </button> */
+}
+{
+   /*    )} */
+}
+{
+   /*    {!session.data?.user && ( */
+}
+{
+   /*       <button className="m-2 p-2 bg-blue-300" onClick={() => signIn()}> */
+}
+{
+   /*          SignIn */
+}
+{
+   /*       </button> */
+}
+{
+   /*    )} */
+}
+{
+   /* </div> */
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { AppBar } from './components/AppBar'
+import { MainContent } from './components/MainContent'
+import { Section } from './components/Section'
 
 export default function Home() {
    return (
-      <main>
+      <main className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900">
          <AppBar />
+         <MainContent />
+         <Section />
       </main>
    )
 }
